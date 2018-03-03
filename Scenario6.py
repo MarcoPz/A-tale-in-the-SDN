cf106b0b5970abaf26777b83b65dbc7c4539232e
Marco Putzulu <36650492+MarcoPz@users.noreply.github.com>
2018-03-03 01:47:22 +0100

    Update Scenario6.py

--- Scenario6.py
+++ Scenario6.py
@@ -48,23 +48,30 @@ def myNetwork():
     
     print ('SETTING THE VERSION OF OPENFLOW TO BE USED IN EACH ROUTER:\n')
     s1.cmdPrint('ovs-vsctl set Bridge s1 protocols=OpenFlow13')
+    
     print('FOR SAFETY I KILLALL CONTROLLER PREVIOUS:\n')
     c0.cmdPrint('killall controller')
+    
     print('START REST_FIREWALL ON XTERM OF C0 CONTROLLER:\n')
     c0.cmdPrint('ryu-manager ryu.app.rest_firewall &')
-    print('FIREWALL STARTED SET TO CUT OFF ALL COMMUNICATION:\n')
-    #net.pingAll()
+    
+    print('FIREWALL STARTED SET TO CUT OFF ALL COMMUNICATION:\n') #when it start, block all connections
+    net.pingAll() #test it
+    
     print('ENABLE FIREWALL:\n')
     c0.cmdPrint('curl -X PUT http://localhost:8080/firewall/module/enable/0000000000000001 &')
     c0.cmdPrint('curl http://localhost:8080/firewall/module/status  &')
-    print('ADD RULES FOR PINGING BETWEEN H1 AND H2:\n')
-    #c0.cmdPrint(curl -X POST -d '{"nw_src": "10.0.0.1/8", "nw_dst": "10.0.0.2/8", "nw_proto": "ICMP"}' http://localhost:8080/firewall/rules/0000000000000001) QUI c'e' il problema
-    #c0.cmdPrint(curl -X POST -d '{"nw_src": "10.0.0.2/8", "nw_dst": "10.0.0.1/8", "nw_proto": "ICMP"}' http://localhost:8080/firewall/rules/0000000000000001) QUI c'e' il problema
+    
+    #print('ADD RULES FOR PINGING BETWEEN H1 AND H2:\n')
+    #Open an xterm of c0 in the mininet dialog (mininet> xterm c0)
+    #c0.cmdPrint(curl -X POST -d '{"nw_src": "10.0.0.1/8", "nw_dst": "10.0.0.2/8", "nw_proto": "ICMP"}' http://localhost:8080/firewall/rules/0000000000000001) add this rule in c0
+    #c0.cmdPrint(curl -X POST -d '{"nw_src": "10.0.0.2/8", "nw_dst": "10.0.0.1/8", "nw_proto": "ICMP"}' http://localhost:8080/firewall/rules/0000000000000001) add this rule in c0
+    
     print('PING h1->h2\n')
-    #net.pingAll()
+    #net.pingAll() in mininet CLI (mininet> pingAll and you will notice that now h1 ping with success h2, while h3 still not reachable.
    
-    
-    
+    #You have added some rules just for the ICMP protocol, but you can add others related to all protcols.
+   
     CLI(net)
     net.stop()
     
